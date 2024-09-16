ee34d97705a2b3303302f76b6ce55caf6bafa1c3
Ubuntu <ubuntu@ip-172-31-15-243.ap-south-1.compute.internal>
2024-09-16 11:28:20 +0000

    if statement was uploaded

---
+++ ifstatement.sh
@@ -0,0 +1,14 @@
+# This file is used to find the value of the username 
+#
+
+
+echo "enter the user name"
+
+read usename
+
+if [ $username == "sunny" ]
+then
+	echo "entered username is correct"
+else 
+	echo "please enter the correct usernname"
+fi
